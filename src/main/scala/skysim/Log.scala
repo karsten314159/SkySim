d1d5b07a1748223832034cc49e3e6ebd88cdf937
karsten314159 <brox.p@web.de>
2019-08-09 11:17:21 +0200

    log

--- src/main/scala/skysim/Log.scala
+++ src/main/scala/skysim/Log.scala
@@ -1,9 +1,7 @@
 package skysim
 
-import akka.event.Logging
-import akka.event.Logging.LogLevel
 import akka.event.jul.Logger
-import sun.misc.{JavaLangAccess, SharedSecrets}
+//import sun.misc.{JavaLangAccess, SharedSecrets}
 
 object Log {
   final val dev: Boolean = false
@@ -26,15 +24,18 @@ object Log {
   }
 
   def getCaller(throwable: Throwable): (String, String, Int) = {
-    val access: JavaLangAccess = SharedSecrets.getJavaLangAccess
+    //val access = SharedSecrets.getJavaLangAccess
 
-    val depth: Int = access.getStackTraceDepth(throwable)
 
+    val frames = throwable.getStackTrace
+    val depth: Int = frames.length // access.getStackTraceDepth(throwable)
     for (ix <- 1.until(depth)) {
       // Calling getStackTraceElement directly prevents the VM
       // from paying the cost of building the entire stack frame.
-      val frame = access.getStackTraceElement(throwable, ix)
+      //val frame = access.getStackTraceElement(throwable, ix)
+      val frame: StackTraceElement = frames(ix)
       val cname = frame.getClassName
+
       return (cname, frame.getMethodName, frame.getLineNumber)
     }
     ("", "", 0)
